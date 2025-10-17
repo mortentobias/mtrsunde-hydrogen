4689563abc30383e88dad708d8bb74b8c2f9587e
Morten T. R. Sunde <mtrsunde@gmail.com>
2025-10-17 22:15:22 +0200

    edit index

--- hydrogen/app/routes/_index.tsx
+++ hydrogen/app/routes/_index.tsx
@@ -3,17 +3,12 @@ import {Link} from 'react-router';
 export default function Index() {
   return (
     <div className="mx-auto p-12 prose prose-xl prose-a:text-blue-500">
-      <section>
-        <h1 className="text-3xl font-bold">Home</h1>
-      </section>
-
-      <section>
-          <Link className="text-blue-500 underline" to="/collections/all">
-            All Products
-          </Link>
-      </section>
-
-      <section>askjdfklasfjkl</section>
+      <h1 className="text-3xl font-bold">Home</h1>
+      <p>
+        <Link className="text-blue-500 underline" to="/collections/all">
+          All Products
+        </Link>
+      </p>
     </div>
   );
 }
